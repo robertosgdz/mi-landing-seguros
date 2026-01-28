e128d95a907a16efc82ad53c66b6201929545cdb
robertosgdz <robertosegadodiaz@gmail.com>
2026-01-28 12:11:42 +0100

    servidor astro netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
     build: {
